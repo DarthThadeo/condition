b723d807bfcf28b9ccbefbc294eb58dde97d09c7
Thadeo <74535236+DarthThadeo@users.noreply.github.com>
2023-09-22 11:42:19 +0200

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+"use strict"
+
+let a = 4;// j'ai creer la variable a
+let b = 4;// j'ai creer la variable b
+let c = 3;// j'ai creer la variable c
+
+switch (a) {
+    case b:
+        console.log("égal à b");//si a est égal b faire console log "égal à b"
+        break;
+    case c:
+        console.log("égal à c")//si a est égal b faire console log "égal à b"
+        break;
+    default:
+        console.log("égal à rien")//par default dans le console log y a "égal à rien"
+}
